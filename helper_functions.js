bab3a59536ed42a6f6b844fb20cda4bcfa5fca23
Keith Wong <keith.wong3672@gmail.com>
2017-03-16 16:23:56 -0700

    Clean up code

--- helper_functions.js
+++ helper_functions.js
@@ -8,7 +8,6 @@ var toggleImage = function (event, data) {
 };
 
 var getAPIQuery = function (startIdx, count, category) {
-  console.log('*********************getAPIQuery Function************************');
   var url = 'http://ign-apis.herokuapp.com/';
   var query = url + category + '?startIndex=' + startIdx + '&count=' + count;
   return query;
@@ -43,8 +42,6 @@ var formatTime = function(duration) {
 };
 
 var parseData = function (videosOrArticles, start, end) {
-  console.log('********************inside popData function****************');
-  console.log('Start Index is: ', start);
   end = end || start + 10;
   // need to add more code here for when to add more content vs just empting content out
   if (start === 0) {
@@ -53,14 +50,11 @@ var parseData = function (videosOrArticles, start, end) {
 
   for (var index = start; index < end; index++) {
     var data = videosOrArticles[index];
-    console.log(data);
-
     var title = data.metadata.name || data.metadata.headline;
     var description = data.metadata.description || data.metadata.subHeadline;
     var duration = formatTime(data.metadata.duration);
     var imgUrl = data.thumbnails[0].url;
     var srcSet = data.thumbnails[0].url + ' ' + data.thumbnails[0].width + 'w, ' + data.thumbnails[1].url + ' ' + data.thumbnails[1].width + 'w, ' + data.thumbnails[2].url + ' ' + data.thumbnails[2].width + 'w';
-    console.log('srcset is :', srcSet);
     var url = data.metadata.url;
 
     displayContent(index + 1, title, description, duration, srcSet, url);
