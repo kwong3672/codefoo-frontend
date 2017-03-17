9b060e9339a6eaaf87a67a0bab6f729471a1e73a
Keith Wong <keith.wong3672@gmail.com>
2017-03-16 17:11:26 -0700

    Add additional comments

--- get_content.js
+++ get_content.js
@@ -1,11 +1,15 @@
 var getContent = function (category, startIdx, count) {
+  // create default values if parameter not passed in
   startIdx = startIdx || 0;
   count = count || 20;
   category = category || 'videos'; 
+  // ensure that category is lowercase
   category = category.toLowerCase();
+  // call function to get query string for API get request
   var APIQuery = getAPIQuery(startIdx, count, category);
 
-
+  // if category picked is not active category reset start index
+  // and clear preloaded data
   if (currentCategory !== category) {
     currentCategory = category;
     startIdx = 0;
@@ -22,8 +26,10 @@ var getContent = function (category, startIdx, count) {
         parseData(response.data, 0, 10);
         toggleArticleType();
       }
-      preloadData = preloadData.concat(response.data);
 
+      // make additional API requests and save for faster loading of
+      // additional content and allow searching of content
+      preloadData = preloadData.concat(response.data);
       if (startIdx < 300) {
         getContent(currentCategory, response.startIndex + 20, 20);
       }
